d223c0f916dcfa331fbadd4d54bcf5391069810b
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2026-07-18 22:35:04 +0200

    feature: environment vars and README.md

--- 05-NOC/src/app.ts
+++ 05-NOC/src/app.ts
@@ -1,9 +1,12 @@
-import { Server } from "./presentation/server.js";
-
+import { envs } from "./config/plugins/envs.plugin.js";
 (async () => {
   await main();
 })();
 
 async function main() {
-  await Server.start();
+  // await Server.start();
+  console.log("process.env.MAILER_EMAIL", envs.MAILER_EMAIL);
+  console.log("process.env.MAILER_PASSWORD", envs.MAILER_PASSWORD);
+  console.log("process.env.PORT", envs.PORT);
+  console.log("process.env.PROD", envs.PROD);
 }
