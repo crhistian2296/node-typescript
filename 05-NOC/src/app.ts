8457ac40f53416ce0cdb603b68a84bd31af66dcc
crhistian2296 <garcia.crhistian@correonivalle.edu.co>
2026-08-27 03:13:05 +0200

    feature: integration withn docker and mongo

--- 05-NOC/src/app.ts
+++ 05-NOC/src/app.ts
@@ -1,10 +1,15 @@
-import { Server } from "./presentation/server.js";
+import { envs } from "./config/plugins/envs.plugin.js";
+import { MongoDatabase } from "./data/mongo/index.js";
 (async () => {
   await main();
 })();
 
 async function main() {
-  await Server.start();
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME,
+  });
+  // await Server.start();
   // console.log("process.env.MAILER_EMAIL", envs.MAILER_EMAIL);
   // console.log("process.env.MAILER_PASSWORD", envs.MAILER_PASSWORD);
   // console.log("process.env.PORT", envs.PORT);
--- 05-NOC/src/config/plugins/envs.plugin.ts
+++ 05-NOC/src/config/plugins/envs.plugin.ts
@@ -8,4 +8,8 @@ export const envs = {
   MAILER_PASSWORD: env.get("MAILER_PASSWORD").required().asString(),
   MAILER_SECRET_KEY: env.get("MAILER_SECRET_KEY").required().asString(),
   PROD: env.get("PROD").default("false").asBool(),
+  MONGO_URL: env.get("MONGO_URL").required().asString(),
+  MONGO_DB_NAME: env.get("MONGO_DB_NAME").required().asString(),
+  MONGO_USER: env.get("MONGO_USER").required().asString(),
+  MONGO_PASS: env.get("MONGO_PASS").required().asString(),
 };
